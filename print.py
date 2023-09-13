cc05dce63e97b40edb7fbac4921e0a592869cd54
vanKeluj <Julek.van@outlook.com>
2023-09-13 10:35:54 +0200

    Update print.py

--- print.py
+++ print.py
@@ -1,5 +1,5 @@
-print("Jestem w lekcji informatyki")
-print("Pracuje w trybie skryptowym")
-print("Julek")
-print("cos")
-print("masło")
+liczba1 = input("Podaj pierwszą liczbe: ")
+liczba2 = int(input("Podaj pierwszą liczbe: "))
+print("Suma liczb: {0:s} + {1:d} wynosi {2:d}"
+      .format(liczba1,liczba2,
+              int(liczba1)+int(liczba2)))
