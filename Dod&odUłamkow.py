844cc14344f2be935f816400e1d921cbbcd61b67
vanKeluj <Julek.van@outlook.com>
2024-03-13 10:15:54 +0100

    0 nuh uh

--- Dod&odUłamkow.py
+++ Dod&odUłamkow.py
@@ -21,34 +21,49 @@ l2 = int(input("Podaj drugi licznik: "))
 m2 = int(input("Podaj drugi minanownik: "))
 
 def dod(a, b, c, d):
-    e = (a*d + c*b)
-    f= (b*d)
-    return skrocenie(e,f)
+    if b != 0 and d != 0:
+        e = (a*d + c*b)
+        f= (b*d)
+        return skrocenie(e,f)
+    else:
+        return False
 
 def NWWdod(a, b, c, d):
-    nww = NWW(b, d)
-    e = (a * (nww / b) + c * (nww / d))
-    f = nww
-    return skrocenie(e,f)
+    if b != 0 and d != 0:
+        nww = NWW(b, d)
+        e = (a * (nww / b) + c * (nww / d))
+        f = nww
+        return skrocenie(e,f)
+    else:
+        return False
 
 def mnoz(a, b, c, d):
-    nwd1 = NWD(a, d)
-    nwd2 = NWD(b, c)
-    e = ((a / nwd1) * (c / nwd2))
-    f = ((b / nwd2) * (d / nwd1))
-    return skrocenie(e,f)
+    if b != 0 and d != 0:
+        nwd1 = NWD(a, d)
+        nwd2 = NWD(b, c)
+        e = ((a / nwd1) * (c / nwd2))
+        f = ((b / nwd2) * (d / nwd1))
+        return skrocenie(e,f)
+    else:
+        return False
 
 def dziel(a, b, d, c):
-    nwd1 = NWD(a, d)
-    nwd2 = NWD(b, c)
-    e = ((a / nwd1) * (c / nwd2))
-    f = ((b / nwd2) * (d / nwd1))
-    return skrocenie(e,f)
+    if b != 0 and d != 0 and  c!=0:
+        nwd1 = NWD(a, d)
+        nwd2 = NWD(b, c)
+        e = ((a / nwd1) * (c / nwd2))
+        f = ((b / nwd2) * (d / nwd1))
+        return skrocenie(e,f)
+    else:
+        return False
 
 def od(a, b, c, d):
-    e = (a*d - c*b)
-    f= (b*d)
-    return skrocenie(e,f)
+    if b != 0 and d != 0:
+        e = (a*d - c*b)
+        f= (b*d)
+        return skrocenie(e,f)
+    else:
+        return False
 
 
 print("Dodawanie: ", dod(l1, m1, l2, m2))
