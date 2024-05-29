64d1c5f03a5b7be2cce49c346c885d2c037bd799
vanKeluj <Julek.van@outlook.com>
2024-05-29 08:52:55 +0200

    Update Binary.py

--- Binary.py
+++ Binary.py
@@ -11,6 +11,20 @@ def decimal_to_binary(n):
     
     return binary_return
 
+def binary_to_decimal(n):
+    decimal = 0
+    power = 1
+    while n>0:
+        rem = n%10
+        n = n//10
+        decimal += rem*power
+        power = power*2
+        
+    return decimal
+
+
+x = int(input("Enter a binary number: "))
+print("Decimal representation:", binary_to_decimal(x))
 
 n= int(input("Podaj Liczbę: "))
 print("Twoja liczba w binarnym to:", decimal_to_binary(n))
