80e945cf8ec5b526f104aec2ef4bf5785ccfa829
Julek <126201111+vanKeluj@users.noreply.github.com>
2024-11-27 11:59:33 +0100

    Update Sortowanie_tekstu_zadania.py

--- Sortowanie_tekstu_zadania.py
+++ Sortowanie_tekstu_zadania.py
@@ -51,3 +51,28 @@ print("Sorted string using insertion sort:", insertion_sort(string))
 9876 123 76547 4343 898 2345
 
 '''
+
+def zad_2(string):
+    string = string.replace(" ","").lower()
+    li = []
+    n = len(string)
+    
+    for i in range(n):
+        li.append(string[i])
+    
+    for i in range(1,n):
+        temp = li[i]
+        j = i - 1
+        while j >= 0 and li[j] > temp:
+            li[j + 1] = li[j]
+            j -= 1
+        li[j+1] = temp
+    
+    string_return = ""
+    for i in range(n):
+        string_return += li[i]
+    
+    return string_return
+
+zad2string = "9876 123 76547 4343 898 2345"
+print(zad_2(zad2strig))
